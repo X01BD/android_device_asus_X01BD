c1be730671240546a660053e246ae8e18b84cc4d
Sean Tranchetti <stranche@codeaurora.org>
2018-03-28 09:29:04 -0600

    X01BD: change data properties to comply with Treble
    Treble changes in Android now require stricter naming conventions for
    vendor properties and also impose stricter requirements on using
    properties as actionable triggers in init scripts. This change
    updates the properties used by data modules to follow these new
    guidelines.
    
    Change-Id: I40fd28f6f3727c326fd13d6bc189a6ab5fd0ff37
    CRs-Fixed: 2215428

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -75,9 +75,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Data modules
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.iwlan.enable=true \
-    persist.data.mode=concurrent \
+    persist.vendor.data.mode=concurrent \
     persist.data.netmgrd.qos.enable=true \
-    ro.use_data_netmgrd=true
+    ro.vendor.use_data_netmgrd=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
