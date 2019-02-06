f35426028fba9404e147a34cbaf690fe162e347f
kubersharma001 <kubersharma001@gmail.com>
2019-02-06 09:34:38 +0530

    X01BD: adapt for LineageOS
    Change-Id: I978729962417d5e54dff5e0f87123555bd90c303

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_X01BD.mk
+    $(LOCAL_DIR)/lineage_X01BD.mk
--- lineage_X01BD.mk
+++ lineage_X01BD.mk
@@ -19,14 +19,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common arrow stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_X01BD
+PRODUCT_NAME := lineage_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M2
