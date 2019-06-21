d9484f236fb717f52eff1d0c5db67905ac9b2903
kubersharma001 <kubersharma001@gmail.com>
2019-06-21 19:56:16 +0530

    X01BD: fix few typos in device makefile
    Change-Id: Ieed3706e7d81ab9cc2332bd2a3173916a718f561

--- device.mk
+++ device.mk
@@ -260,14 +260,14 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service \
     com.android.nfc_extras \
     NfcNci \
     SecureElement \
-    Tag \
+    Tag
 
 # NFC-Secure Element
 PRODUCT_PACKAGES += \
@@ -278,7 +278,6 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
-
 
 # OMX
 PRODUCT_PACKAGES += \
