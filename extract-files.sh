c945731f9c73093d04e230d31bde4b67bbb7107a
kubersharma001 <kubersharma001@gmail.com>
2019-06-24 15:57:11 +0530

    X01BD: slighty update the extraction script
    Change-Id: Ia74f2a09a4396b4d9757b75e4f6e70d0a3988cb8

--- extract-files.sh
+++ extract-files.sh
@@ -61,19 +61,18 @@ extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
 extract "$MY_DIR"/proprietary-files-twrp.txt "$SRC" "$SECTION"
 
 BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+TWRP_QSEECOMD="$BLOB_ROOT"/recovery/root/sbin/qseecomd	
+TWRP_GATEKEEPER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.gatekeeper@1.0-service-qti	
+TWRP_KEYMASTER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.keymaster@3.0-service-qti	
 
-TWRP_QSEECOMD="$BLOB_ROOT"/recovery/root/sbin/qseecomd
-TWRP_GATEKEEPER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.gatekeeper@1.0-service-qti
-TWRP_KEYMASTER="$BLOB_ROOT"/recovery/root/sbin/android.hardware.keymaster@3.0-service-qti
+sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_QSEECOMD"	
+sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_GATEKEEPER"	
+sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_KEYMASTER"	
 
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_QSEECOMD"
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_GATEKEEPER"
-sed -i "s|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g" "$TWRP_KEYMASTER"
+IMSCMSERVICE="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice.xml	
+IMSCMSERVICE_1_1="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice_1_1.xml	
 
-IMSCMSERVICE="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice.xml
-IMSCMSERVICE_1_1="$BLOB_ROOT"/vendor/etc/permissions/com.qualcomm.qti.imscmservice_1_1.xml
-
-sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE"
-sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE_1_1"
+sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE"	
+sed -i "s|/system/framework/|/vendor/framework/|g" "$IMSCMSERVICE_1_1
 
 . "$MY_DIR"/setup-makefiles.sh
