6d4ae4dfc663e3113fbcd71cb9142e037cd289d2
kubersharma001 <kubersharma001@gmail.com>
2019-07-15 21:37:32 +0530

    X01BD: build few more audio packages
    Change-Id: Ibf704258fcc6fd66c8ac2db15f182d4e076f1ec5

--- device.mk
+++ device.mk
@@ -72,8 +72,11 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@4.0 \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl \
